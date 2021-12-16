a9a5a324331c5584661b9d0a4687518ea3743c21
Gabriel Couture <gacou54@gmail.com>
2021-12-16 09:25:46 -0500

    Correction du bug d<accumulation de termes dans le csv

--- scripts/regroup_terms_results.py
+++ scripts/regroup_terms_results.py
@@ -12,28 +12,27 @@ import pandas
 from pytrombone import Cache
 
 CSV_FILEPATH = ''
-CACHE_PATH = './data/tmp.db'
+CACHE_PATH = ''
 ALL_TERMS_FILEPATH = ''
 
 # Récupération des arguments
-optlist, _ = getopt.getopt(sys.argv[1:], '', ['csv_file=', 'all_terms_filepath='])
+optlist, _ = getopt.getopt(sys.argv[1:], '', ['csv_file=', 'cache_path=', 'all_terms_filepath='])
 
 for opt, value in optlist:
     if 'csv_file' in opt:
         CSV_FILEPATH = value
 
+    if 'cache_path' in opt:
+        CACHE_PATH = value
+
     if 'all_terms_filepath' in opt:
         ALL_TERMS_FILEPATH = value
 
-if not CSV_FILEPATH or not ALL_TERMS_FILEPATH:
-    raise ValueError('Les paramètres csv_file et all_terms_filepath doivent tous être fournies')
+if not CSV_FILEPATH or not CACHE_PATH or not ALL_TERMS_FILEPATH:
+    raise ValueError('Les paramètres csv_file, cache_path et all_terms_filepath doivent tous être fournies')
 
+# Récupération des termes de tous les documents
 df = pandas.read_csv(ALL_TERMS_FILEPATH)
-
-new_df = pandas.DataFrame(
-    columns=['term', 'totalRawFreq', 'relativeFreqAvg', 'zscoreAvg', 'zscoreRatioAvg', 'tfidfAvg', 'totalTermsCount']
-)
-terms_without_duplicates = ()
 
 # Initialisation de la cache, qui marque si oui ou non un terme à été traité
 cache = Cache(CACHE_PATH)
@@ -49,22 +48,22 @@ for term in df['term'].unique():
 
     # On trouve toutes les occurrences des termes par document.
     term_rows = df.loc[df['term'] == term]
-    new_df = new_df.append({
-        'term': term,
-        'totalRawFreq': sum(term_rows['rawFreq']),
-        'relativeFreqAvg': sum(term_rows['relativeFreq']) / len(term_rows['relativeFreq']) / 1_000_000,  # Pour obtenir une valeur normalisée
-        'zscoreAvg': sum(term_rows['zscore']) / len(term_rows['zscore']),
-        'zscoreRatioAvg': sum(term_rows['zscoreRatio']) / len(term_rows['zscoreRatio']),
-        'tfidfAvg': sum(term_rows['tfidf']) / len(term_rows['tfidf']),
-        'totalTermsCount': sum(term_rows['totalTermsCount']),
-    }, ignore_index=True)
+    term_df = pandas.DataFrame(data={
+        'term': [term],
+        'totalRawFreq': [sum(term_rows['rawFreq'])],
+        'relativeFreqAvg': [sum(term_rows['relativeFreq']) / len(term_rows['relativeFreq']) / 1_000_000],  # Pour obtenir une valeur normalisée
+        'zscoreAvg': [sum(term_rows['zscore']) / len(term_rows['zscore'])],
+        'zscoreRatioAvg': [sum(term_rows['zscoreRatio']) / len(term_rows['zscoreRatio'])],
+        'tfidfAvg': [sum(term_rows['tfidf']) / len(term_rows['tfidf'])],
+        'totalTermsCount': [sum(term_rows['totalTermsCount'])],
+    })
 
     # Écriture dans un fichier csv
     if os.path.exists(CSV_FILEPATH):
-        new_df.to_csv(CSV_FILEPATH, mode='a', header=False, index=False)
+        term_df.to_csv(CSV_FILEPATH, mode='a', header=False, index=False)
 
     else:
-        new_df.to_csv(CSV_FILEPATH, index=False)
+        term_df.to_csv(CSV_FILEPATH, index=False)
 
     # Marque le terme comme traité
     cache.mark_as_processed(term)
